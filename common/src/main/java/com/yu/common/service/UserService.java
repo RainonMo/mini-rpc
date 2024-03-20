647111548f0cb2b0b2f41dc34a661649b12ade6a
momo <mochunt@163.com>
2024-03-20 22:53:32 +0800

    first commit

---
+++ common/src/main/java/com/yu/common/service/UserService.java
@@ -0,0 +1,16 @@
+package com.yu.common.service;
+
+import com.yu.common.model.User;
+
+public interface UserService {
+    /**
+     * 获取用户
+     * @param user
+     * @return
+     */
+    User getUser(User user);
+
+    default short getNumber(){
+        return 1;
+    }
+}
